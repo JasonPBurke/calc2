f06615f63a0244819aaaa93f79b6b1c987ae64df
Jason Burke <jpburke77@gmail.com>
2022-10-23 18:08:20 -0500

    completed calculator application

--- script.js
+++ script.js
@@ -6,47 +6,87 @@ let = currentOperator = '';
 let firstValue = '';
 let secondValue = '';
 let equalsPressed = false;
+let captureSecondNumber = false;
+let lastKeyPressed;
 
 
 keys.addEventListener('click', (key) => {
 
-
+  
   if(key.target.classList.contains('number')) {
-    if(display.textContent === '0' || currentOperator || equalsPressed) {
+    if(display.textContent.includes('%') && captureSecondNumber === 'false') return;
+    if(display.textContent === '0' || captureSecondNumber || equalsPressed) { // || currentOperator || equalsPressed) {
       display.textContent = key.target.textContent;
+      captureSecondNumber = false;
+      equalsPressed = false;
     } else {
       display.textContent += key.target.textContent;
     }
   }
-
+  
   if(key.target.classList.contains('operator')) {
+    if(lastKeyPressed) {
+      if(lastKeyPressed.classList.contains('operator')) return;
+    }
+    if(currentOperator) {
+      secondValue = display.textContent;
+      display.textContent = operate(firstValue, currentOperator, secondValue);
+
+    }
     currentOperator = key.target.textContent;
     firstValue = display.textContent;
+    captureSecondNumber = true;
+    keys.querySelector('.decimal').disabled = false;
+    keys.querySelector('.percent').disabled = false;
   }
 
   if(key.target.classList.contains('equals')) {
+    keys.querySelector('.decimal').disabled = false;
+    keys.querySelector('.percent').disabled = false;
     equalsPressed = true;
     secondValue = display.textContent;
+    if(firstValue.includes('%')) firstValue = parseFloat(firstValue)/100;
+    if(secondValue.includes('%')) secondValue = parseFloat(secondValue)/100;
     display.textContent = operate(firstValue, currentOperator, secondValue);
+    if(display.textContent === '') {
+      display.textContent = '0';
+    } else if(display.textContent === 'Infinity' ) {
+      display.textContent = 'You a big dummy, Fry';
+    }
+    currentOperator = '';
+    firstValue = '';
+    secondValue = '';
   }
+
+  if(key.target.classList.contains('clear')) {
+    firstValue = '';
+    secondValue = '';
+    currentOperator = '';
+    display.textContent = '0';
+  }
+
+  if(key.target.classList.contains('back')) {
+    if(lastKeyPressed.classList.contains('equals')) return;
+    display.textContent = display.textContent.slice(0, -1);
+    if(display.textContent === '') display.textContent = '0';
+  }
+
+
+  if(key.target.classList.contains('decimal')) key.target.disabled = true;
+  if(key.target.classList.contains('percent')) key.target.disabled = true;
+  lastKeyPressed = key.target;
 })
 
 
 
-
-
-
-
-
-
 function operate(a, operator, b) {
-  a = parseInt(a);
-  b = parseInt(b);
-  console.log(operator);
+  a = parseFloat(a);
+  b = parseFloat(b);
+  if(!a) return b;
   if(operator === '+') return add(a, b);
   if(operator === '-') return subtract(a, b);
-  if(operator === '*') return multiply(a, b);
-  if(operator === '/') return divide(a, b);
+  if(operator.charCodeAt(0) === 215) return multiply(a, b);
+  if(operator.charCodeAt(0) === 247) return divide(a, b);
 }
 
 function add(a, b) {
